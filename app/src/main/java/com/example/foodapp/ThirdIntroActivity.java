7b89ea9eaaea48322da1e7e3ba12afc6b9dc56e2
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-15 14:03:49 -0400

    added swipe event third intro activity

--- app/src/main/java/com/example/foodapp/ThirdIntroActivity.java
+++ app/src/main/java/com/example/foodapp/ThirdIntroActivity.java
@@ -2,13 +2,35 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class ThirdIntroActivity extends AppCompatActivity {
+    float x1, x2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_intro);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+
+                if (x1 < x2) {
+                    startActivity(new Intent(ThirdIntroActivity.this, SecondIntroActivity.class));
+                }
+
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 }
