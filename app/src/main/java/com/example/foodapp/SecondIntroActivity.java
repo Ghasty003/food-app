c52b3579100236b0c5c57cb2359417a2e354110e
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-15 13:40:12 -0400

    added swipe event to second intro activity

--- app/src/main/java/com/example/foodapp/SecondIntroActivity.java
+++ app/src/main/java/com/example/foodapp/SecondIntroActivity.java
@@ -2,13 +2,34 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class SecondIntroActivity extends AppCompatActivity {
+    float x1, x2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_intro);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+
+                if (x1 < x2) {
+                    startActivity(new Intent(SecondIntroActivity.this, FirstIntroActivity.class));
+                }
+
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 }
