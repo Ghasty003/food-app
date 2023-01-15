e835c60e4add99bf080c7c640f0a0b7c6366b74d
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-15 13:15:31 -0400

    added swipe event to views

--- app/src/main/java/com/example/foodapp/FirstIntroActivity.java
+++ app/src/main/java/com/example/foodapp/FirstIntroActivity.java
@@ -2,13 +2,36 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class FirstIntroActivity extends AppCompatActivity {
+    float x1, x2, y1, y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_intro);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+
+                if (x2 < x1) {
+                    startActivity(new Intent(FirstIntroActivity.this, SecondIntroActivity.class));
+                }
+
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 }
