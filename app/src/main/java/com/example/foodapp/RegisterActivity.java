5d8d5ec52d39deca2243512ef4816ffde5eaa51c
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-17 19:37:11 -0400

    added click event to login textview

--- app/src/main/java/com/example/foodapp/RegisterActivity.java
+++ app/src/main/java/com/example/foodapp/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView login = findViewById(R.id.login);
+
+        login.setOnClickListener(view -> {
+            startActivity(new Intent(this, LoginActivity.class));
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        });
     }
 }
