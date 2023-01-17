6bb7adaacb2e8bd87d866d0ead450a7652159922
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-17 19:34:03 -0400

    added click event to register button

--- app/src/main/java/com/example/foodapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/foodapp/SplashScreenActivity.java
@@ -15,10 +15,16 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         MaterialButton Login = findViewById(R.id.login);
+        MaterialButton Register = findViewById(R.id.register);
 
         Login.setOnClickListener(view -> {
             startActivity(new Intent(this, LoginActivity.class));
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        });
+
+        Register.setOnClickListener(view -> {
+            startActivity(new Intent(this, RegisterActivity.class));
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
         });
     }
 }
