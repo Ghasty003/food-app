3fb417b4b372a3628b24b667d7f4777d14525337
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-17 12:58:18 -0400

    added click listener to login button

--- app/src/main/java/com/example/foodapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/foodapp/SplashScreenActivity.java
@@ -2,7 +2,10 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.button.MaterialButton;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        MaterialButton Login = findViewById(R.id.login);
+
+        Login.setOnClickListener(view -> {
+            startActivity(new Intent(this, LoginActivity.class));
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        });
     }
 }
