80c9d18f982ba09b80bb9cf9d851e673da467b84
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-19 17:35:48 -0400

    added method to load user data

--- app/src/main/java/com/example/foodapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/foodapp/SplashScreenActivity.java
@@ -4,10 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.button.MaterialButton;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
 public class SplashScreenActivity extends AppCompatActivity {
+    private final String FILE_NAME = "user.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +35,44 @@ public class SplashScreenActivity extends AppCompatActivity {
             startActivity(new Intent(this, RegisterActivity.class));
             overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
         });
+
+        if (!Objects.equals(loadUserData(), "")) {
+            Log.d("MY_APP", loadUserData());
+        } else {
+            Log.d("MY_APP", "no data");
+        }
+    }
+
+    private String loadUserData() {
+        FileInputStream fileInputStream = null;
+        String data = "";
+
+        try {
+            fileInputStream = openFileInput(FILE_NAME);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            String text;
+            while ((text = bufferedReader.readLine()) != null) {
+                stringBuilder.append(text);
+            }
+
+            data = stringBuilder.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return data;
     }
 }
