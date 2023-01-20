b8db45d6b1c25bbdb6760253393dfcfb3a473975
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-20 18:00:56 -0400

    added item selected listener to bottom navigation

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.foodapp.fragments.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        homeFragment = new HomeFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                    return true;
+            }
+            return false;
+        });
     }
 }
