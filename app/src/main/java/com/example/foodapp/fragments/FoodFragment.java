7f1fde4f909198de5fcd1da0e600be87cafb0ba2
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-20 18:49:02 -0400

    updated

--- app/src/main/java/com/example/foodapp/fragments/FoodFragment.java
+++ app/src/main/java/com/example/foodapp/fragments/FoodFragment.java
@@ -2,16 +2,20 @@ package com.example.foodapp.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.foodapp.R;
+import com.example.foodapp.ViewPager2Adapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class FoodFragment extends Fragment {
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
