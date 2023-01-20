6054c087985d36eada0e0a15d7d1c94d7aac74b7
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-20 18:39:53 -0400

    added viewpager2 adapater

---
+++ app/src/main/java/com/example/foodapp/ViewPager2Adapter.java
@@ -0,0 +1,29 @@
+package com.example.foodapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.foodapp.fragments.FoodFragment;
+
+public class ViewPager2Adapter extends FragmentStateAdapter {
+
+    public ViewPager2Adapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            default:
+                return new FoodFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5;
+    }
+}
